059c4c022c7dfbec08cc77721ef7967b5c8c3273
Annika Ronkainen <t5roan00@students.oamk.fi>
2018-03-15 09:00:21 +0200

    lisätty kommentti

--- app/src/main/java/num/pad/MainActivity.java
+++ app/src/main/java/num/pad/MainActivity.java
@@ -3,6 +3,9 @@ package num.pad;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+//aluksi git harjoittelua
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
